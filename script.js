7a3d793413da09d05c20922e6e7bf71f04545f40
ArnavGupta-codes <guptaarnav631@gmail.com>
2025-02-06 10:52:03 +0530

    New Changes

--- script.js
+++ script.js
@@ -11,18 +11,20 @@ async function searchUser() {
       if (data.message === "Not Found") {
           info.innerHTML = "❌ Invalid User: User does not exist"; //I am unable to use fontawesome coz javascript.
       } else {
-          info.innerHTML = `
-              <img src="${data.avatar_url}" width="100" height="100" style="border-radius: 50%;">
-              <p><strong>User:</strong> ${data.login}</p>
-              <hr><br>
-              <p><strong>Bio:</strong> ${data.bio || "No bio available"}</p>
-          
-              <p><strong>Public Repositories:</strong> ${data.public_repos || "No Public Repo"
-              }</p>
-              <p><strong>Followers:</strong> ${data.followers}</p>
-              <p><strong>Following:</strong> ${data.following}</p>
-          `;
-      }
+        info.innerHTML = `
+        <br>
+            <img src="${data.avatar_url}" width="100" height="100" style="border-radius: 50%;">
+            <p><strong>User:</strong> <a href="${data.html_url}" target="_blank">${data.login}</a></p>
+            <hr><br>
+            <p><strong>Bio:</strong> ${data.bio || "No bio available"}</p>
+            <p><strong>Public Repositories:</strong> <a href="${data.html_url}?tab=repositories" target="_blank">
+                ${data.public_repos || "No Public Repo"}
+            </a></p>
+            <p><strong>Followers:</strong> ${data.followers}</p>
+            <p><strong>Following:</strong> ${data.following}</p>
+        `;
+    }
+    
   } catch (error) {
       info.innerHTML = "⚠️ Error fetching data";
       console.error("Error:", error);
